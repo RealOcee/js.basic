ee1e5b83c60203d0a3e045c17b131bfffd166cf3
Real Ocee <onasosi@gmail.com>
2022-07-07 16:46:26 +0100

    assignment

--- script.js
+++ script.js
@@ -115,12 +115,18 @@ x--;
 console.log(x);
 /*/
 
-var heightJohn = 23
-var heightMark = 20
-var BMI(10 )
-console.log(heightJohn + heightMark)
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
 
+var massJohn = 92;
+var heightJohn = 1.95;
 
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn)
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log ('Is Mark\'s BMI higher than John\'s? '+ markHigherBMI)
 
 
 
@@ -211,36 +217,6 @@ console.log(heightJohn + heightMark)
 /**********************
 * Basic operators
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
