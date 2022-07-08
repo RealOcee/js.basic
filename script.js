ea83af97d1c96d11ce170f6505c04001591ac5b1
Real Ocee <onasosi@gmail.com>
2022-07-08 16:38:01 +0100

    nigeria

--- script.js
+++ script.js
@@ -115,18 +115,20 @@ x--;
 console.log(x);
 /*/
 
-var massMark = 78; // kg
+
+//var massMark = 78; // kg
 var heightMark = 1.69; // meters
 
-var massJohn = 92;
-var heightJohn = 1.95;
+//var massJohn = 92;
+//var heightJohn = 1.95;
 
-var BMIMark = massMark / (heightMark * heightMark);
-var BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn)
+//var BMIMark = massMark / (heightMark * heightMark);
+//var BMIJohn = massJohn / (heightJohn * heightJohn);
+//console.log(BMIMark, BMIJohn)
 
-var markHigherBMI = BMIMark > BMIJohn;
-console.log ('Is Mark\'s BMI higher than John\'s? '+ markHigherBMI)
+//var markHigherBMI = BMIMark > BMIJohn;
+//console.log ('Is Mark\'s BMI higher than John\'s? '+ markHigherBMI
+/*/
 
 
 
