a26a20a2fc0f1aba6a6a99b6eb8377a9a04a1ea2
Real Ocee <onasosi@gmail.com>
2022-09-12 03:01:20 +0100

    if else statement

--- if else statement.js
+++
@@ -1,8 +0,0 @@
-var firstName = 'John';
-var civilStatus = 'single';
-
-if (civilStatus === 'married') {
-    console.log(firstName + 'is married!');
-} else {
-    console.log(firstName + ' will hopefully marry soon :)');
-}
--- script.js
+++ script.js
@@ -117,7 +117,7 @@ console.log(x);
 
 
 //var massMark = 78; // kg
-var heightMark = 1.69; // meters
+// var heightMark = 1.69; // meters
 
 //var massJohn = 92;
 //var heightJohn = 1.95;
@@ -134,6 +134,40 @@ var heightMark = 1.69; // meters
 
 
 
+/*** IF ELSE STATEMENT */
+
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + 'is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + 'is married');
+} else {
+    console.log(firstName + 'will hopefully marry soon :)');
+}
+
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Marks\'s.');
+}
 
 
 
