4055a0a83d1737069ff01780f6b4a00a3d4e0c4a
Real Ocee <onasosi@gmail.com>
2022-09-12 03:31:24 +0100

    added

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var if = 23;
 
 
 /********************
-* Variable mutation and tyope coercion
+* Variable mutation and type coercion
 */
 
 /*
@@ -137,37 +137,47 @@ console.log(x);
 /*** IF ELSE STATEMENT */
 
 
+// var firstName = 'John';
+// var civilStatus = 'single';
+
+// if (civilStatus === 'married') {
+//     console.log(firstName + 'is married!');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon :)');
+// }
+
+// var isMarried = true;
+// if (isMarried) {
+//     console.log(firstName + 'is married');
+// } else {
+//     console.log(firstName + 'will hopefully marry soon :)');
+// }
+
+
+// var massMark = 78; // kg
+// var heightMark = 1.69; // meters
+
+// var massJohn = 92;
+// var heightJohn = 1.95;
+
+// var BMIMark = massMark / (heightMark * heightMark);
+// var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// if (BMIMark > BMIJohn) {
+//     console.log('Mark\'s BMI is higher than John\'s.');
+// } else {
+//     console.log('John\'s BMI is higher than Marks\'s.');
+// }
+
+
+
+
+/******************************
+ * Boolean Logic
+ */
+
 var firstName = 'John';
-var civilStatus = 'single';
-
-if (civilStatus === 'married') {
-    console.log(firstName + 'is married!');
-} else {
-    console.log(firstName + ' will hopefully marry soon :)');
-}
-
-var isMarried = true;
-if (isMarried) {
-    console.log(firstName + 'is married');
-} else {
-    console.log(firstName + 'will hopefully marry soon :)');
-}
-
-
-var massMark = 78; // kg
-var heightMark = 1.69; // meters
-
-var massJohn = 92;
-var heightJohn = 1.95;
-
-var BMIMark = massMark / (heightMark * heightMark);
-var BMIJohn = massJohn / (heightJohn * heightJohn);
-
-if (BMIMark > BMIJohn) {
-    console.log('Mark\'s BMI is higher than John\'s.');
-} else {
-    console.log('John\'s BMI is higher than Marks\'s.');
-}
+var age = 
 
 
 
