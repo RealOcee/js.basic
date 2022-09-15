48c27903a69a5a33c98225de233590e040f85579
Real Ocee <onasosi@gmail.com>
2022-09-15 12:19:47 +0100

    boolean logic

--- script.js
+++ script.js
@@ -177,7 +177,17 @@ console.log(x);
  */
 
 var firstName = 'John';
-var age = 
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) { //Between 13 and 20 === age >= 13 AND age < 20
+    console.log(firstName + ' is a teenager.');
+} else if(age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man.');
+}
 
 
 
