4b4039550f2f7424c61d112b08c5db0549aaa9f7
Real Ocee <onasosi@gmail.com>
2022-10-03 10:43:55 +0100

    updated

--- script.js
+++ script.js
@@ -166,87 +166,40 @@ Operator Precedence
 // }
 
 
+
+
 // Boolean logic
 
-const firstName = 'John';
-const age = 20;
-
-if (age < 13) {
-    console.log(firstName + ' is a boy.');
-} else if(age >= 13 && age < 20){
-    console.log(firstName + ' is a teenager.');
-} else if (age >= 20 && age < 30) {
-    console.log(firstName + ' is a young man. ');
-}else {
-    console.log(firstName + ' is a man.');
-}
 
 
+// const firstName = 'John';
+// const age = 20;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/***********************
- * The Ternary Operator and Switch statements
- */
-
-// var firstName = 'Andrew';
-// var age = 14;
-
-// age >= 18 ? console.log(firstName + ' drink beer.')
-// :console.log(firstName + ' drinks juice.');
-
-
-// var firstName = 'Andrew';
-// var age = 18;
-
-// age >= 18 ? console.log(firstName + ' can vote.')
-// : console.log(firstName + ' can not vote.');
-
-
-// Switch statement
-
-// var job = 'teacher';
-// switch (job) {
-//     case 'teacher':
-//         console.log(firstName + ' teaches kids how to code');
-//         break;
-//     case 'driver':
-//         console.log(firstName + ' drives an uber in Lagos');
-//         break;
-//     case 'designer':
-//         console.log(firstName + 'designs beautiful websites');
-//         break;
-//     case ''
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.');
+// } else if(age >= 13 && age < 20){
+//     console.log(firstName + ' is a teenager.');
+// } else if (age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man. ');
+// }else {
+//     console.log(firstName + ' is a man.');
 // }
 
+
+
+
+
+// Ternary Operator and Switch Statements
+ 
+const firstName = 'John';
+const age = 21;
+
+// Ternary Operator 
+age >= 18 ? console.log(firstName + ' drinks beer. ')
+: console.log(firstName + ' drinks juice ');
+
+const drink = age >= 18 ? 'beer ' : 'juice ';
+console.log(drink);
 
 
 
