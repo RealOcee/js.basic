078ca6dce5566203721a267b4b10d266bcf48f69
Real Ocee <onasosi@gmail.com>
2022-10-10 13:45:22 +0100

    Objects & Methods

--- script.js
+++ script.js
@@ -444,30 +444,54 @@ Operator Precedence
 
 
 
+
+
 // Object and Properties 
+// const john = {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     job: 'Teacher',
+//     birthYear: 1999,
+//     family: ['Jane','Elo', 'Favour'],
+//     isMarried: 'false'
+// };
+
+// console.log(john.firstName);
+// console.log(john.lastName);
+// console.log(john['birthYear']);
+
+// const x = 'job';
+// console.log(john[x]);
+
+// john.job = 'designer';
+// john['isMarried'] = true;
+// console.log(john);
+
+// const jane = new Object();
+// jane.firstName = 'John';
+// jane.lastName = 'smith';
+// jane['job'] = 'teacher';
+
+// console.log(jane);
+
+
+
+
+
+//Object and Method
 const john = {
     firstName: 'John',
     lastName: 'Smith',
     job: 'Teacher',
-    birthYear: 1999,
+    birthYear: 2000,
     family: ['Jane','Elo', 'Favour'],
-    isMarried: 'false'
+    isMarried: 'false',
+    calcAge: function() {
+        this.age = 2040 - this.birthYear;
+    }
 };
-
-console.log(john.firstName);
-console.log(john.lastName);
-console.log(john['birthYear']);
-
-const x = 'job';
-console.log(john[x]);
-
-john.job = 'designer';
-john['isMarried'] = true;
+john.calcAge();
 console.log(john);
-
-
-
-
 
 
 
