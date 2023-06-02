66c74979e73549aaa75feacd31e0daafde814a2d
Real Ocee <onasosi@gmail.com>
2023-06-02 15:05:34 +0100

    Updated loop

--- script.js
+++ script.js
@@ -538,6 +538,10 @@ Operator Precedence
 
 // Loops & Iteration
 
+
+// for loop
+
+/*
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
@@ -547,10 +551,34 @@ for (let i = 0; i < john.length; i++) {
     console.log(john[i]);
 }
 
+
+// while loop
+
+
 let i = 0
 while (i < john.length) {
     console.log(john[i]);
     i++;
+} */
+
+
+// Continue and break statement
+
+let john = [ 'Andrew', 'James', 'Henry', 1999, 'faith', false, 'blue'];
+for (let i = 0; i < john.length; i++) {
+    if (typeof john[1] !== 'string') continue;
+    console.log(john[i]);
+}
+
+
+for (let i = 0; i < john.length; i++) {
+    if (typeof john[1] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (let i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
 }
 
 //Loops prevent repetitions of code but still getting same functionality as it.
